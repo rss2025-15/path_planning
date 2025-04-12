b5defa52f5f954c5da014ec2caea3f0994497f06
Jing Cao <jcao0715@gmail.com>
2025-04-12 16:57:26 -0400

    initial a*

--- path_planning/trajectory_planner.py
+++ path_planning/trajectory_planner.py
@@ -6,6 +6,7 @@ from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped, PoseArray
 from nav_msgs.msg import OccupancyGrid
 from .utils import LineTrajectory
 import numpy as np
+import heapq
 
 class Cell:
     def __init__(self):
@@ -80,15 +81,90 @@ class PathPlan(Node):
         self.DOWNSAMPLED_COLS = self.downsampled_map.shape[1]
 
     def pose_cb(self, pose):
-        raise NotImplementedError
+        self.pos = [pose.pose.pose.position.x, pose.pose.pose.position.y]
 
     def goal_cb(self, msg):
-        raise NotImplementedError
+        self.trajectory.clear()
+        self.plan_path(self.pos, [msg.pose.position.x, msg.pose.position.y], self.map, self.map_data)
 
-    def plan_path(self, start_point, end_point, map):
+    def plan_path(self, start_point, end_point, map, map_data):
+        map_info = map.info
+
+        # convert map to pixel coordinates
+        start = self.map_to_grid(start_point, map_info)
+        goal = self.map_to_grid(end_point, map_info)
+
+        path = self.a_star(start, goal, self.downsampled_map)
+        if path is None:
+            self.get_logger().info("No path found")
+            return
+        
+        self.get_logger("Path found")
+        for point in path:
+            self.trajectory.addPoint(self.grid_to_map(point, map_info))
+            
         self.traj_pub.publish(self.trajectory.toPoseArray())
         self.trajectory.publish_viz()
 
+
+    # HELPER FUNCTIONS
+    def is_valid(self, position):
+        return  0 <= position[0] < self.DOWNSAMPLED_ROWS and 0 <= position[1] < self.DOWNSAMPLED_COLS
+
+    def is_obstacle(self, grid, position):
+        return not grid[position[0]][position[1]] == 0
+    
+    def is_goal(self, position, goal):
+        return position == goal
+    
+    def map_to_grid(self, pos, map_info):
+        grid_x = int((pos[0] - map_info.origin.position.x) / map_info.resolution)
+        grid_y = int((pos[1] - map_info.origin.position.y) / map_info.resolution)
+        return (grid_x, grid_y)
+    
+    def grid_to_map(self, grid_pos, map_info):
+        x = grid_pos[0] * map_info.resolution + map_info.origin.position.x + map_info.resolution / 2.0
+        y = grid_pos[1] * map_info.resolution + map_info.origin.position.y + map_info.resolution / 2.0
+        return (x, y)
+    
+    def heuristic(self, start, goal):
+        # euclidean distance
+        return np.linalg.norm(np.array(start) - np.array(goal))
+
+    
+    def a_star(self, start, goal, grid):
+        """everything in grid coordinates"""
+        open_list = []
+        heapq.heappush(open_list, (0, start))
+        came_from = {}
+        g_score = {start: 0}
+        f_score = {start: self.heuristic(start, goal)}
+
+        while open_list:
+            _, current = heapq.heappop(open_list)
+            if self.is_goal(current, goal):
+                # reconstruct path
+                path = []
+                while current in came_from:
+                    path.append(current)
+                    current = came_from[current]
+                path.append(start)
+                path.reverse()
+                return path # includes start node
+            
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+                neighbor = (current[0] + dx, current[1] + dy)
+                if not self.is_valid(neighbor) or self.is_obstacle(grid, neighbor):
+                    continue
+
+                tentative_g_score = g_score[current] + self.heuristic(current, neighbor)
+                if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
+                    came_from[neighbor] = current
+                    g_score[neighbor] = tentative_g_score
+                    f_score[neighbor] = tentative_g_score + self.heuristic(neighbor, goal)
+                    heapq.heappush(open_list, (f_score[neighbor], neighbor))
+
+        return None  # no path found
 
 def main(args=None):
     rclpy.init(args=args)

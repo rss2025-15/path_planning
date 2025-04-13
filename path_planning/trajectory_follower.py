e055a5af3a7e2c56f40776ab4bac1386db635283
megg05 <megantseng55@gmail.com>
2025-04-13 02:24:33 +0000

    follows first segment only

--- path_planning/trajectory_follower.py
+++ path_planning/trajectory_follower.py
@@ -2,6 +2,7 @@ import rclpy
 from ackermann_msgs.msg import AckermannDriveStamped
 from geometry_msgs.msg import PoseArray, PoseWithCovarianceStamped, PoseStamped
 from nav_msgs.msg import Odometry
+from std_msgs.msg import Header
 from rclpy.node import Node
 from visualization_msgs.msg import Marker
 
@@ -11,6 +12,7 @@ from .utils import LineTrajectory
 
 import numpy as np
 import math
+import rclpy
 
 
 class PurePursuit(Node):
@@ -25,7 +27,7 @@ class PurePursuit(Node):
         self.odom_topic = self.get_parameter('odom_topic').get_parameter_value().string_value
         self.drive_topic = self.get_parameter('drive_topic').get_parameter_value().string_value
 
-        self.lookahead = 1.  # FILL IN #
+        self.lookahead = .5  # FILL IN #
         self.speed = 1.  # FILL IN #
         self.wheelbase_length = .46  # FILL IN #
 
@@ -64,12 +66,14 @@ class PurePursuit(Node):
                                                  self.fake_callback,
                                                  1)
 
+        self.lookahead_pub = self.create_publisher(Marker, "/lookahead_pt", 1)
+
 
     def init_callback(self, init_msg):
         self.initialized_pose = True
 
     def pose_callback(self, estimated_robot_msg):
-        self.get_logger().info(f'{type(estimated_robot_msg)} is the message type' )
+        # self.get_logger().info(f'{type(estimated_robot_msg)} is the message type' )
         # orientation = euler_from_quaternion([odometry_msg.pose.pose.orientation.x, odometry_msg.pose.pose.orientation.y, odometry_msg.pose.pose.orientation.z, odometry_msg.pose.pose.orientation.w])[-1]
         orientation = euler_from_quaternion([
         estimated_robot_msg.pose.orientation.x,
@@ -84,7 +88,7 @@ class PurePursuit(Node):
             orientation
         ])
 
-        self.get_logger().info(f'current robot pose: {robot_pose}')
+        # self.get_logger().info(f'current robot pose: {robot_pose}')
         if self.initialized_traj and self.initialized_pose:
             # FINDING CLOSEST SEGMENT
             # https://stackoverflow.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment/1501725#1501725
@@ -106,10 +110,12 @@ class PurePursuit(Node):
             # https://codereview.stackexchange.com/questions/86421/line-segment-to-circle-collision-algorithm/86428#86428
             lookahead_angle = None
             segments_checked = 0
+            segments_got = 0
             too_far = False
-            # self.get_logger().info(f"dist_to_segments {dist_to_segments}")
-            while lookahead_angle is None and segments_checked < len(self.trajectory.points)-1:
-
+            t_intersected_pts = np.empty((0,))
+            t_intersected_coeffs = []
+            while segments_checked < len(self.trajectory.points)-1 and segments_got < 2:
+                self.get_logger().info(f"dist to segments {dist_to_segments}")
                 i = np.argmin(dist_to_segments) # closest segment currently
                 segments_checked += 1
                 # self.get_logger().info(f"{i}")
@@ -125,31 +131,59 @@ class PurePursuit(Node):
                 disc = b**2 - 4*a*c
 
                 if disc < 0: # no intersection like ever even if the segment was extended
-                    too_far = True
+                    # too_far = True
+                    self.get_logger().info(f"too far is true")
                     break
 
                 # parametrized intersection pts
                 t_pts = np.array([(-b + math.sqrt(disc))/(2*a), (-b - math.sqrt(disc))/(2*a)])
-                t_intersected_pts = t_pts[(t_pts >= 0) & (t_pts <= 1)]
-                self.get_logger().info(f"t_intersected_pts{t_intersected_pts}")
+                if t_pts[(t_pts >= 0) & (t_pts <= 1)].any():
+                    t_intersected_pts = np.append(t_intersected_pts, t_pts[(t_pts >= 0) & (t_pts <= 1)][0])
+                    t_intersected_coeffs.append((s_1, segment_v))
+                    self.get_logger().info(f"t_intersected_pts {t_intersected_pts}")
+                    self.get_logger().info(f"t_intersected_coeffs {t_intersected_coeffs}")
 
-                if len(t_intersected_pts) == 0: # no intersection within segment
-                    dist_to_segments[i] = float('inf')
-                    continue
+                segments_got += 1
+                
+                dist_to_segments[i] = float('inf')
+                
+            if t_intersected_pts.any():
+                pt = t_intersected_coeffs[-1][0] + t_intersected_pts[-1]*(t_intersected_coeffs[-1][1])
 
-                pts = s_1 + t_intersected_pts[-1]*(segment_v)
-                self.get_logger().info(f"pts{pts}")
-                # angles = np.arctan2(pts[:,1], pts[:,0])
-                # lookahead_angle = angles[np.argmin(np.abs(angles - robot_pose[2]))]
-                lookahead_angle = math.atan2(pts[0] - robot_pose[0], pts[1]-robot_pose[1])
+                map_wrt_robot = self.compute_transform_from_to(tuple(robot_pose), (0, 0, 0))
+                lookahead_pt = self.compose_transforms(map_wrt_robot, (pt[0], pt[1], 0))
+                
 
-            # ACTUAL PURE PURSUIT
-            turn_radius = self.lookahead / (2*math.sin(lookahead_angle)) if not too_far else 0.
-            steer_angle = math.atan(self.wheelbase_length/turn_radius) if not too_far else 0.
-            # self.cmd_speed = max(1.0-math.exp(-self.exp_speed_coeff*(lookahead-self.parking_distance)),self.close_speed) # this is from panos code
-            self.cmd_speed = 1.0
-            self.drive_cmd(steer_angle, self.cmd_speed)
-            self.get_logger().info(f'steering {steer_angle} >:)')
+                self.get_logger().info(f"lookahead pt in robot frame{lookahead_pt}")
+                lookahead_angle = math.atan2(lookahead_pt[1], lookahead_pt[0])
+                self.get_logger().info(f"lookahead angle {lookahead_angle}")    
+
+                # ACTUAL PURE PURSUIT
+                turn_radius = self.lookahead / (2*math.sin(lookahead_angle))
+                steer_angle = math.atan(self.wheelbase_length/turn_radius)
+                # self.cmd_speed = max(1.0-math.exp(-self.exp_speed_coeff*(lookahead-self.parking_distance)),self.close_speed) # this is from panos code
+                self.cmd_speed = 1.0
+                self.drive_cmd(steer_angle, self.cmd_speed)
+                self.get_logger().info(f'steering {steer_angle} >:)')
+            else:
+                self.drive_cmd(0.0, 1.0)
+                self.get_logger().info(f'too far, steering {0.0} >:)')
+
+    def compute_transform_from_to(self, from_pos, to_pos):
+        from_x, from_y, from_theta = from_pos
+        to_x, to_y, to_theta = to_pos
+        dx = np.cos(from_theta)*(to_x-from_x)+np.sin(from_theta)*(to_y-from_y)
+        dy = -np.sin(from_theta)*(to_x-from_x)+np.cos(from_theta)*(to_y-from_y)
+        dtheta = to_theta - from_theta
+        return (dx, dy, dtheta)
+
+    def compose_transforms(self, t1, t2):
+        t1_dx, t1_dy, t1_dtheta = t1
+        t2_dx, t2_dy, t2_dtheta = t2
+        t_dx = t1_dx+np.cos(t1_dtheta)*t2_dx-np.sin(t1_dtheta)*t2_dy
+        t_dy = t1_dy+np.sin(t1_dtheta)*t2_dx+np.cos(t1_dtheta)*t2_dy
+        t_dtheta = t1_dtheta+t2_dtheta
+        return (t_dx, t_dy, t_dtheta)
 
     def drive_cmd(self, steer, speed = 1.0):
         drive_cmd_drive = AckermannDriveStamped()
